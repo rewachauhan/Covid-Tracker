48b8d0bc999fa67b12cf0022c0ff0e721556ca59
rewachauhan <rewachauhan79@gmail.com>
2020-04-16 19:59:29 +0530

    map added

---
+++ src/app/api.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import{ Http,Response} from '@angular/http';
+import{ map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: Http){}
+
+    // URL FOR THE SERVER
+    private url="https://raw.githubusercontent.com/amcharts/covid-charts/master/data/json/world_timeline.json"
+    private uri = "https://raw.githubusercontent.com/amcharts/covid-charts/master/data/json/total_timeline.json"
+    // private url = "https://jsonplaceholder.typicode.com/todos/1"
+    private ur = "https://corona.lmao.ninja/v2/historical/India"
+    // FUNCTION FOR GETTING RESPONSE AND CONVERT TO JSON
+    GetData(){
+        return this.http.get(this.url)
+        .pipe(map((res: Response) => res.json()))
+    }
+    addGetData(){
+        return this.http.get(this.uri)
+        .pipe(map((res: Response) => res.json()))
+    }
+    lineData(){
+      return this.http.get(this.ur)
+      .pipe(map((res: Response) => res.json()))
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import {MatCardModule} from '@angular/material/card';
 import {MatSelectModule} from '@angular/material/select';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ChartsModule} from 'ng2-charts';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 @NgModule({
   declarations: [
     AppComponent
@@ -16,7 +18,9 @@ import { FormsModule } from '@angular/forms';
     MatCardModule,
     BrowserAnimationsModule,
     MatSelectModule,
-    FormsModule
+    FormsModule,
+    ChartsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
